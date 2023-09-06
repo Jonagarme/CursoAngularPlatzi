4dee70f184f417ed8e46ae9d04975d716662dd80
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-06 16:51:20 -0500

    clases y estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {Product} from './product.model'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Jonathan';
   age = 22;
   img = 'https://source.unsplash.com/random';
